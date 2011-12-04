d4d686990eade053cd21f23a854457c1c4a03249
Konrad-Podgorski <konrad.podgorski@gmail.com>
2011-12-04 13:42:04 +0100

    polski jezyk, formularz logowania i inne

--- apps/backend/lib/myUser.class.php
+++ apps/backend/lib/myUser.class.php
@@ -2,4 +2,8 @@
 
 class myUser extends sfGuardSecurityUser
 {
+  public function __construct(sfEventDispatcher $dispatcher, sfStorage $storage, $options = array()) {
+    parent::__construct($dispatcher, $storage, $options);
+    $this->setCulture('pl');
+  }
 }
--- apps/frontend/lib/myUser.class.php
+++ apps/frontend/lib/myUser.class.php
@@ -1,5 +1,10 @@
 <?php
 
-class myUser extends sfGuardSecurityUser
-{
+class myUser extends sfGuardSecurityUser {
+
+  public function __construct(sfEventDispatcher $dispatcher, sfStorage $storage, $options = array()) {
+    parent::__construct($dispatcher, $storage, $options);
+    $this->setCulture('pl');
+  }
+
 }
